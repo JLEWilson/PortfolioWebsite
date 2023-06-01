1cbc17d92a7e128c1f2ed18bbb96870154216f38
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2023-06-01 15:30:03 -0800

    add base colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#000000',
+        text: '#ffffff',
+      },
+    },
   },
   plugins: [],
-};
+}
