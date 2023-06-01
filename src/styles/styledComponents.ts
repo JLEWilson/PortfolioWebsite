a094b97383d6295d9034e4f5de28b1623a00bbde
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2023-06-01 15:32:36 -0800

    these are not staying, just testing

---
+++ src/styles/styledComponents.ts
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const PrimaryContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`
+export const Header = styled.text`
+  font-size: 50px;
+`
