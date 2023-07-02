3e7c80293d1ab80aa2bcda9fccea76462ec2f497
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2023-07-01 20:22:07 -0800

    add  api provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import { Typography } from './Typography'
 import { store } from './store'
 import About from './components/about'
 import { Provider } from 'react-redux'
+import { ApiProvider } from '@reduxjs/toolkit/dist/query/react'
+import { githubApi } from './features/githubSlice'
 function App() {
   return (
     <Provider store={store}>
-      <div className={join('text-center bg-background h-full m-0')}>
-        <Typography variant='h1' className={join('text-text')}>
-          Jacob Wilson
-        </Typography>
-        <About />
-      </div>
+      <ApiProvider api={githubApi}>
+        <div className={join('text-center bg-background h-full m-0')}>
+          <Typography variant='h1' className={join('text-text')}>
+            Jacob Wilson
+          </Typography>
+          <About />
+        </div>
+      </ApiProvider>
     </Provider>
   )
 }
