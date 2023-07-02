acacb7ca03fa77490d4331989892df2a945a5136
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2023-07-01 20:25:53 -0800

    update reducer

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
-
+import { githubApi } from './features/githubSlice'
 export const store = configureStore({
   reducer: {
-    projects: projectsReducer,
+    github: githubApi.reducer,
+  },
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
