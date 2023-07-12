f1f3e9466d2ee9edb4203a79818f937cd9a68ade
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2023-07-11 20:04:01 -0800

    Change project list

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,17 @@ import { Typography } from './Typography'
 import { store } from './store'
 import About from './components/about'
 import { Provider } from 'react-redux'
-import { ApiProvider } from '@reduxjs/toolkit/dist/query/react'
-import { githubApi } from './features/githubSlice'
+import ProjectListWrapper from './components/projectListWrapper'
 function App() {
   return (
     <Provider store={store}>
-      <ApiProvider api={githubApi}>
-        <div className={join('text-center bg-background h-full m-0')}>
-          <Typography variant='h1' className={join('text-text')}>
-            Jacob Wilson
-          </Typography>
-          <About />
-        </div>
-      </ApiProvider>
+      <div className={join('text-center bg-background m-0')}>
+        <Typography variant='h1' className={join('text-text')}>
+          Jacob Wilson
+        </Typography>
+        <About />
+        <ProjectListWrapper />
+      </div>
     </Provider>
   )
 }
