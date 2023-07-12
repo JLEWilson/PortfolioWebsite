3cd3113a9b8f30cca9ac4a4af794771d7967eea4
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2023-07-11 20:04:55 -0800

    add base project

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Typography } from '../Typography'
+import { join } from '../utils'
 
-type projectPropTypes = {}
+type projectPropTypes = {
+  props?: string
+}
 
 const Project = (props: projectPropTypes) => {
-  return <div>project</div>
+  return (
+    <Typography variant='body' className={join('text-text')}>
+      {' '}
+      Example Project
+    </Typography>
+  )
 }
 
 export default Project
