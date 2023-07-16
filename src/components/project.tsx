4db9652c37e1e97273ba0354dc03c1ce5bf9fbfa
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2023-07-16 15:37:11 -0800

    add basic styling

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -1,17 +1,48 @@
 import React from 'react'
 import { Typography } from '../Typography'
 import { join } from '../utils'
+import { repository } from '../features/RepositoryList'
 
 type projectPropTypes = {
-  props?: string
+  repoData: repository
 }
 
-const Project = (props: projectPropTypes) => {
+const Project = ({ repoData }: projectPropTypes) => {
   return (
-    <Typography variant='body' className={join('text-text')}>
-      {' '}
-      Example Project
-    </Typography>
+    <div className={join('bg-green-700 border-solid border-black border')}>
+      <Typography variant='h2' className={join('m-2')}>
+        {repoData.name}
+      </Typography>
+      {repoData.links.map((link, index) => (
+        <a href={link.url} key={'link ' + index} className={join('mx-4')}>
+          {link.name}
+        </a>
+      ))}
+      <hr className='mb-2' />
+      <Typography variant='h4' className={join('mb-4')}>
+        {repoData.description}
+      </Typography>
+      <Typography variant='h5'>Languages</Typography>
+      <div className={join('flex flex-wrap justify-center')}>
+        {repoData.languages.map((language, index) => (
+          <Typography variant='body' key={'language ' + index} className='mx-4'>
+            {language}
+          </Typography>
+        ))}
+      </div>
+      <Typography variant='h5'>Technologies</Typography>
+      <div className={join('flex flex-wrap justify-center ')}>
+        {repoData.technologies.map((tech, index) => (
+          <Typography
+            variant='body'
+            key={'technology ' + index}
+            className='mx-4'
+          >
+            {tech}
+          </Typography>
+        ))}
+      </div>
+    </div>
   )
 }
 
