f2147fbd36faa30abcc9debbeff2813158982416
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2023-07-16 15:37:18 -0800

    moved away from redux api

--- src/components/projectList.tsx
+++ src/components/projectList.tsx
@@ -1,35 +1,25 @@
 import React from 'react'
-import { useLazyGetReposByPageQuery } from '../features/githubSlice'
+
 import { Typography } from '../Typography'
 import { join } from '../utils'
+import Project from './project'
+import { repository } from '../features/RepositoryList'
 
 type projectPropTypes = {
-  totalRepos: number
+  reposList: repository[]
 }
 
 const ProjectList = (props: projectPropTypes) => {
-  const [repos, setRepos] = React.useState<any>([])
-  const totalPages = Math.ceil(props.totalRepos / 100)
-  console.log(totalPages)
-  const [trigger] = useLazyGetReposByPageQuery()
-
-  React.useEffect(() => {
-    const fetchAllRepositories = async () => {
-      for (let i = 0; i < totalPages; i++) {
-        const data = await trigger(i)
-        const tempArr = []
-        tempArr.push(...repos)
-        tempArr.push(...data.data)
-        setRepos(tempArr)
-      }
-    }
-    fetchAllRepositories()
-  }, [repos, totalPages, trigger])
-
   return (
-    <Typography variant='body' className={join('text-text')}>
-      {JSON.stringify(repos)}
-    </Typography>
+    <div
+      className={join(
+        'w-9/12 m-auto border-dashed border-white border text-text',
+      )}
+    >
+      {props.reposList.map((repo, index) => (
+        <Project repoData={repo} key={index}></Project>
+      ))}
+    </div>
   )
 }
 
