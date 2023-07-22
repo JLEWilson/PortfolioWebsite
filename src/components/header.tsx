3d40429cdd99bc058b7bfad147a865419ae092b6
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2023-07-22 15:52:51 -0800

    add header

---
+++ src/components/header.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { join } from '../utils'
+import { Typography } from '../Typography'
+
+function Header() {
+  return (
+    <div className='mx-10'>
+      <Typography
+        variant='h1'
+        className={join('font-Changa font-normal text-text text-left pt-5')}
+      >
+        Jacob Wilson
+      </Typography>
+      <Typography
+        variant='h1'
+        className={join(
+          'text-text text-left',
+          'font-Changa font-normal',
+          '-mt-2',
+          'rotate-x-180 translate-x-5',
+          '[mask-image:_repeating-linear-gradient(transparent,_transparent_3px,_white_5px,_white_5px),linear-gradient(_transparent_50%,_white_90%)]',
+        )}
+      >
+        Jacob Wilson
+      </Typography>
+    </div>
+  )
+}
+
+export default Header
