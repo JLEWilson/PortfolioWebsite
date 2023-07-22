0927e2f466816028f1fb45cb7c387cd8496c4e57
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2023-07-22 15:52:06 -0800

    add animation, need to dry

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,46 @@ module.exports = {
   content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      animation: {
+        bigSlide: 'text-slide-lg 8s infinite',
+        smallSlide: 'text-slide-sm 8s infinite',
+      },
+      keyframes: {
+        'text-slide-lg': {
+          '0%': { top: '0' },
+          '20%': { top: '-192px' },
+          '40%': { top: '-384px' },
+          '60%': { top: '-576px' },
+          '80%': { top: '-768px' },
+          '100%': { top: '-936px' },
+        },
+        'text-slide-sm': {
+          '0%': { top: '0' },
+          '20%': { top: '-90px' },
+          '40%': { top: '-180px' },
+          '60%': { top: '-270px' },
+          '80%': { top: '-360px' },
+          '100%': { top: '-450px' },
+        },
+      },
       colors: {
-        background: '#000000',
-        text: '#ffffff',
+        background: '#001C30',
+        primary: '#176B87',
+        secondary: '#64CCC5',
+        text: '#DAFFFB',
+      },
+      skew: {
+        140: '140deg',
+      },
+      fontFamily: {
+        Roboto: ['Roboto', 'serif'],
+        Changa: ['Changa', 'sans-serif'],
+      },
+      lineHeight: {
+        'extra-loose': '2.5',
+        '12': '3rem',
       },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-3d')({ legacy: true })],
 }
