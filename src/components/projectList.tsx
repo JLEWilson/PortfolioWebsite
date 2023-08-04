57e1fb8ae3b5c45eee32e9eaff0c1f7204087bfb
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2023-08-04 10:15:18 -0800

    styling

--- src/components/projectList.tsx
+++ src/components/projectList.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { join } from '../utils'
 import Project from './project'
 import { repository } from '../consts/RepositoryList'
+import { Typography } from '../Typography'
 
 type projectPropTypes = {
   reposList: repository[]
@@ -9,11 +10,17 @@ type projectPropTypes = {
 
 const ProjectList = (props: projectPropTypes) => {
   return (
-    <div className={join('w-9/12 mt-60 space-y-60 pb-60 m-auto text-text')}>
-      {props.reposList.map((repo, index) => (
-        <Project repoData={repo} key={index} />
-      ))}
-    </div>
+    <section className='mt-60 font-Changa font-light text-text  md:mx-20 mx-5'>
+      <Typography variant='h1' className='snap-center'>
+        Projects
+      </Typography>
+      <hr className='my-2 mx-[30vw]' />
+      <div className={join('w-9/12 space-y-60 pb-60  m-auto')}>
+        {props.reposList.map((repo, index) => (
+          <Project repoData={repo} key={index} />
+        ))}
+      </div>
+    </section>
   )
 }
 
