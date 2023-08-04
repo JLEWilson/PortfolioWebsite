af85bd77317d8dbca5165a50f9c3d90cb2a549b8
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2023-08-04 10:14:39 -0800

    styling

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,26 +1,24 @@
 import React from 'react'
 import { Typography } from '../Typography'
-import { center } from '../styles/tailwindStyleGroups'
 import { join } from '../utils'
 
 const About = () => {
   return (
-    <div
-      className={join(
-        center,
-        'md:mt-52 mt-40 snap-center flex flex-col font-Changa font-light md:mx-20 mx-5',
-      )}
+    <section
+      className={join('md:mt-52 mt-40 font-Changa font-light md:mx-20 mx-5')}
     >
-      <Typography variant='h1' className='text-text mb-5'>
+      <Typography variant='h1' className='text-text'>
         About Me
       </Typography>
+      <hr className='my-2 mx-[30vw] h-2' />
       <Typography variant='h2' className='text-secondary'>
-        I'm a perpetually curious self-taught coder. My journey began learning C#
-        in order to develop video games as a hobby, and I'm now eager to find my
-        first tech role after completing a coding bootcamp and interning at a
-        tech startup in Portland. Let's create something great together!
+        Perpetually curious, I am originally a self-taught coder. My journey
+        began learning C# in order to develop video games as a hobby, I went on
+        to complete a coding bootcamp to learn best practices and gain some
+        direction. Having interned at a tech start up I am now eager to find my
+        first tech role. Let's create something great together!
       </Typography>
-    </div>
+    </section>
   )
 }
 
