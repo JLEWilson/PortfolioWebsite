b6391f587cb653eee1ea230066f7f83b4176528c
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2023-11-01 21:52:56 -0700

    add basic onclick function for modal

--- src/components/navMenu.tsx
+++ src/components/navMenu.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
-import { MenuIcon } from '../consts/icons'
+import { MenuIcon, CloseIcon } from '../consts/icons'
 import Links from './links'
 
 function NavMenu() {
+  const [modalOpen, setModalOpen] = React.useState(false)
+  const handleClick = () => {
+    setModalOpen(!modalOpen)
+  }
   return (
     <div className='fill-text absolute top-10 right-10'>
-      <div className='md:hidden'>{MenuIcon({ height: 24, width: 24 })}</div>
+      <div className='md:hidden bg-black' onClick={handleClick}>{
+        modalOpen ? CloseIcon({ height: 24, width:24 }) : MenuIcon({ height: 24, width: 24 })
+      }</div> 
+     
       <div className='hidden md:block'>
         <Links />
       </div>
