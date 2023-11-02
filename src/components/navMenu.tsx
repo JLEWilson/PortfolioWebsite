658b560625b9b23e2e164181a5d1510bcf437387
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2023-11-02 07:29:21 -0700

    add preliminary modal

--- src/components/navMenu.tsx
+++ src/components/navMenu.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { MenuIcon, CloseIcon } from '../consts/icons'
 import Links from './links'
+import Modal from './modal'
+
+//This file should be renamed and made more single funcion, possibly a nav container but for now we can 
+//flush it out and separate
 
 function NavMenu() {
   const [modalOpen, setModalOpen] = React.useState(false)
@@ -8,11 +12,22 @@ function NavMenu() {
     setModalOpen(!modalOpen)
   }
   return (
-    <div className='fill-text absolute top-10 right-10'>
-      <div className='md:hidden bg-black' onClick={handleClick}>{
-        modalOpen ? CloseIcon({ height: 24, width:24 }) : MenuIcon({ height: 24, width: 24 })
-      }</div> 
-     
+    <div className=''>
+      {
+        modalOpen &&  
+        <div className='absolute top-10 right-10 bg-secondary w-20 h-20 '>
+          <Modal />
+        </div>
+      }
+      <div onClick={handleClick}>
+        {
+          modalOpen ? 
+              CloseIcon({ height: 24, width:24 }) 
+           
+          : MenuIcon({ height: 24, width: 24 })
+        }
+      </div> 
+
       <div className='hidden md:block'>
         <Links />
       </div>
