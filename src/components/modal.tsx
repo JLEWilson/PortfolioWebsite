b44f20a3ada79ebe9d18b43f341bb2730fa83a08
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2023-11-02 07:29:38 -0700

    abstract out modal component, will WIP

---
+++ src/components/modal.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+// interface modalProps {
+//     width: string | number 
+//     height:string | number
+//     alignment: {
+//         top: string | number
+//         right: string | number
+//         bottom: string | number
+//         left: string | number
+//     }
+// }
+
+const Modal = () => {
+    return (
+        <div className=''>
+            
+        </div>
+    );
+};
+
+export default Modal;
