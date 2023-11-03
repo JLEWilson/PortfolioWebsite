6fde56fdedffda8f7d9944e6d4815f9e0f4c2fc4
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2023-11-03 09:10:54 -0700

    WIP modal implementation

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { CloseIcon } from '../consts/icons';
 
 // interface modalProps {
-//     width: string | number 
+//     width: string | number
 //     height:string | number
 //     alignment: {
 //         top: string | number
@@ -11,15 +12,28 @@ import React from 'react';
 //     }
 // }
 
-const Modal = ({ open, onClose, children}) => {
-    return (
-        //backdrop
-        <div className={`fixed inset-0 justify-center items-center transition-colors
-            ${open ? "visible bg-black/20" : "invisible"}`}>
-        {/* modal */}
-            <div className={`bg-white rounded-xl shado p-6 transition-all`}></div>
-        </div>
-    );
-};
+function Modal({ open, onClose, children }: any) {
+  return (
+    // backdrop
+    <div
+      className={`fixed inset-0 justify-center items-center transition-colors
+            ${open ? 'visible bg-black/20' : 'invisible'}`}
+    >
+      {/* modal */}
+      <div
+        className={`bg-white rounded-xl shado p-6 transition-all
+            ${open ? 'scale-100 opacity-100' : 'scale-125 opacity-0'}`}
+      >
+        <button
+          onClick={onClose}
+          className="absolute top-2 right-2 p-1 roudned-lg text-text bg-primary hover:bg-secondary"
+        >
+          {CloseIcon({ height: 24, width: 24 })}
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+}
 
 export default Modal;
--- src/components/navMenu.tsx
+++ src/components/navMenu.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { MenuIcon, CloseIcon } from '../consts/icons'
+import { MenuIcon } from '../consts/icons'
 import Links from './links'
 import Modal from './modal'
 
@@ -10,15 +10,12 @@ function NavMenu() {
   const [modalOpen, setModalOpen] = React.useState(false)
   return (
     <div className=''>
-      {
-        modalOpen &&  
-        <div className='absolute top-10 right-10 bg-secondary w-20 h-20 '>
+        <div className='absolute top-10 right-10 fill-secondary'>
           <Modal open={modalOpen} onClose={() => setModalOpen(false)}><h1>This is text</h1></Modal>
-        </div>
-      }
-      <div onClick={() => setModalOpen(true)}>     
+      <button onClick={() => setModalOpen(true)}>     
         {MenuIcon({ height: 24, width: 24 })}
-      </div> 
+      </button> 
+        </div>
 
       <div className='hidden md:block'>
         <Links />
