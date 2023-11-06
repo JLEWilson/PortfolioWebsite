be2d5fc9aa37ba5cf8f5aa57f2cad8da6e7f12b8
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2023-11-05 17:37:36 -0800

    Linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { join } from './utils'
+import React from 'react'
+import join from './utils'
 import About from './components/about'
 import ProjectList from './components/projectList'
 import REPOSITORY_LIST from './consts/RepositoryList'
 import Header from './components/header'
-import Links from './components/links'
 import Title from './components/title'
 import NavMenu from './components/navMenu'
-import useMediaQuery from './hooks/MediaQuery'
 
 function App() {
-  const small = useMediaQuery('max-width: 1199px')
   return (
     <div className={join('text-center bg-background min-h-screen m-0 p-0')}>
       <div className={join('md:flex sticky')}>
