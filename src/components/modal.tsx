59185dfa5b6d567fe0fa30c2e685cc802f3fce00
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2023-11-05 17:38:03 -0800

    Linting

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,29 +1,29 @@
-import React from 'react';
-import { CloseIcon } from '../consts/icons';
+import React from 'react'
+import { CloseIcon } from '../consts/icons'
 
 function Modal({ open, onClose, children }: any) {
   return (
     // backdrop
     <div
-      className={`fixed z-10 inset-0 justify-center items-center transition-colors
+      className={`fixed inset-0 justify-center items-center transition-colors
             ${open ? 'visible bg-black/40' : 'invisible'}`}
     >
       {/* modal */}
       <div
-        className={`bg-primary rounded-xl sticky shadow p-6 w-1/3 h-1/2 ml-auto transition-all
+        className={`bg-primary rounded-xl absolute right-0 z-50 shadow p-6 w-1/3 h-1/2 transition-all
             ${open ? 'scale-100 opacity-100' : 'scale-125 opacity-0'}`}
       >
         <button
-          type="button"
+          type='button'
           onClick={onClose}
-          className="absolute top-2 right-2 p-2 rounded-lg text-text bg-primary hover:bg-secondary"
+          className='absolute top-2 right-2 p-2 rounded-lg text-text bg-primary hover:bg-secondary'
         >
           {CloseIcon({ height: 24, width: 24 })}
         </button>
         {children}
       </div>
     </div>
-  );
+  )
 }
 
-export default Modal;
+export default Modal
