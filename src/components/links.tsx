d46828eec04303982db5a282451895418a7f2d63
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2023-11-07 22:00:35 -0800

    WIP fix flex direction

--- src/components/links.tsx
+++ src/components/links.tsx
@@ -2,33 +2,49 @@ import React from 'react'
 import { GithubIcon, LinkedInIcon, ResumeIcon } from '../consts/icons'
 import type { svgProps } from '../consts/icons'
 
+export enum flexDirection {
+  column = 'flex-col',
+  row = 'flex-row',
+}
+// rename these for clarity
+type linksProps = {
+  direction: flexDirection
+  titleOnHover?: boolean
+}
 type linkProps = {
   icon: (props: svgProps) => React.JSX.Element
+  title: string
   url: string
 }
 
 const links: linkProps[] = [
   {
     icon: GithubIcon,
+    title: 'Github',
     url: 'https://github.com/JLEWilson',
   },
   {
     icon: LinkedInIcon,
+    title: 'LinkedIn',
     url: 'https://www.linkedin.com/in/jlewilson/',
   },
   {
     icon: ResumeIcon,
-    url: 'https://docs.google.com/document/d/1dLblGiNMnjIDh27e1ohwJ4iXQBwT3nLxhY6Ms11Q85w/edit?usp=sharing',
+    title: 'Resume',
+    url: 'https://docs.google.com/document/d/1gU99oO5lQuyNzhPUkRKnmzBD8xICoVjHT61CNXidDBM/edit',
   },
 ]
 
-function Links() {
+const Links: React.FC<linksProps> = ({direction, titleOnHover = false}) => {
   return (
-    <ul className='flex space-x-8'>
+    <ul className={`flex space-x-8 ${direction === flexDirection.column ? 'flex-col' : 'flex-row'}`}>
       {links.map((link, index) => (
         <li key={index}>
-          <a className='fill-text' href={link.url}>
+          <a className='group fill-text p-2' href={link.url}>
             {link.icon({ height: 24, width: 24 })}
+            {titleOnHover && <h4 className='hidden group-hover: block'>{ 
+              link.title 
+            }</h4>}
           </a>
         </li>
       ))}
