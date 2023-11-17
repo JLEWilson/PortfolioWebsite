4e046729762f1e49d3b24f635bf821eb35ddb671
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2023-11-16 21:13:17 -0800

    Add hover animations

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -3,9 +3,9 @@ import { useScrollContext } from '../ScrollContext';
 
 const Navigation: React.FC = () => {
   const { scrollToTarget } = useScrollContext();
-  const buttonStyles = "bg-primary text-left indent-1"
+  const buttonStyles = "bg-moon-gradient bg-300% hover:animate-moon-md hover:border hover:border-primary rounded-lg text-left indent-1 p-2"
     return (
-        <nav className='flex flex-col space-y-8 mb-8 p-2 text-text'>
+        <nav className='flex flex-col space-y-12 mt-20 text-text'>
             <button className={buttonStyles} onClick={() => scrollToTarget(0)}>Top</button>
             <button className={buttonStyles} onClick={() => scrollToTarget(1)}>About</button>
             <button className={buttonStyles} onClick={() => scrollToTarget(2)}> Projects</button>
