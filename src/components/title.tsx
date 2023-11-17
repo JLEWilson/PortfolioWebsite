fbb82b966946d6878e9a916ef1ce55a08d121aee
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2023-11-17 14:21:52 -0800

    Linting and clearing up tailwind

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,37 +1,32 @@
 import React, { useRef, useEffect } from 'react'
-import { useScrollContext } from '../ScrollContext';
+import { useScrollContext } from '../ScrollContext'
 import join from '../utils'
 
 function Title() {
-   const ref = useRef<HTMLDivElement>(null);
-  const { registerScrollTarget } = useScrollContext();
+  const ref = useRef<HTMLDivElement>(null)
+  const { registerScrollTarget } = useScrollContext()
 
   useEffect(() => {
-    registerScrollTarget(ref.current);
-  }, [registerScrollTarget]);
+    registerScrollTarget(ref.current)
+  }, [registerScrollTarget])
 
   return (
     <div ref={ref} className='h-[33vh] z-1'>
       <div
         className={join(
           'relative overflow-hidden',
-          'xl:h-[192px] md:h-[90px] h-[36px]',
+          'xl:h-[192px] md:h-[90px] h-[36px] ',
           'snap-center',
-          'rounded',
-          'fixed md:top-1/2 top-3/4 left-1/2',
-          '-translate-x-1/2',
-          'z-1',
+          'rounded -translate-x-1/2 z-1',
+          'md:top-1/2 top-3/4 left-1/2',
         )}
       >
         <h1
           className={join(
-            'font-Roboto font-normal',
+            'font-Roboto bg-primary',
             'text-text xl:text-9xl/normal md:text-6xl/normal text-2xl/normal',
-            'absolute',
-            'top-0 z-1',
-            'bg-primary',
-            'right-1/2',
-            'pl-4 pr-2',
+            'absolute z-1',
+            'top-0 right-1/2 pl-4 pr-2',
             'xl:animate-bigSlide md:animate-slide animate-smallSlide',
           )}
         >
@@ -47,13 +42,10 @@ function Title() {
         </h1>
         <h1
           className={join(
-            'font-Roboto font-normal',
-            'text-text xl:text-9xl/normal md:text-6xl/normal text-2xl/normal',
-            'absolute',
-            'top-0',
-            'bg-secondary',
-            'pl-2 pr-4 z-1',
-            'left-1/2',
+            'font-Roboto',
+            'text-text bg-secondary',
+            'absolute z-1 left-1/2 top-0 pl-2 pr-4',
+            'xl:text-9xl/normal md:text-6xl/normal text-2xl/normal',
           )}
         >
           Developer
